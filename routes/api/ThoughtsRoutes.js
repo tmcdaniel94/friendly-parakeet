d13054dc2b79176afcfa6115984055d41a8f8d97
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-11-23 20:37:23 -0600

    added route data

--- routes/api/ThoughtsRoutes.js
+++ routes/api/ThoughtsRoutes.js
@@ -1,1 +1,21 @@
 const router = require('express').Router();
+
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require('../../controllers/thoughtController.js');
+
+// /api/Thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/Thoughts/:ThoughtId
+router
+  .route('/:ThoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
 
-
-
-
-
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    // addAssignment,
+    // removeAssignment,
+  } = require('../../controllers/userController');
+  
+  // /api/Users
+  router.route('/').get(getUsers).post(createUser);
+  
+  // /api/Users/:UserId
+  router.route('/:UserId').get(getSingleUser).delete(deleteUser);
+  
+  // /api/Users/:UserId/assignments
+//   router.route('/:UserId/assignments').post(addReaction);
+  
+  // /api/Users/:UserId/assignments/:assignmentId
+//   router.route('/:UserId/assignments/:reactionId').delete(removeReaction);
 
 module.exports = router;
